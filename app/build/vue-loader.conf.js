004abab9602cbdc1a22fd26390363f13cf668cc2
redrice <redrice@163.com>
2017-09-15 22:52:34 +0800

    update

--- app/build/vue-loader.conf.js
+++ app/build/vue-loader.conf.js
@@ -3,26 +3,13 @@ var config = require('../config')
 var isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  loaders:{
-      ...utils.cssLoaders({
+  loaders:utils.cssLoaders({
       sourceMap: isProduction
         ? config.build.productionSourceMap
         : config.dev.cssSourceMap,
       extract: isProduction
   }),
-  less: [
-    'vue-style-loader',
-    'css-loader',
-    {
-      loader: 'less-loader',
-      options: {
-        globalVars: {
-          museUiTheme: `'${museUiThemePath}'`,
-        }
-      }
-    }
-  ]
-  },
+  
   transformToRequire: {
     video: 'src',
     source: 'src',
--- app/build/webpack.base.conf.js
+++ app/build/webpack.base.conf.js
@@ -4,21 +4,13 @@ var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
 
-const museUiThemePath = path.join(
-  __dirname,
-  'node_modules',
-  'muse-ui',
-  'src/styles/themes/variables/default.less'
-)
-
-const vuxLoader = require('vux-loader')
 
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
-const originalConfig=module.exports = {
+module.exports = {
   entry: {
     app: './src/main.js'
   },
@@ -38,10 +30,7 @@ const originalConfig=module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /muse-ui.src.*?js$/,
-        loader: 'babel-loader'
-      },
+      
       {
         test: /\.vue$/,
         loader: 'vue-loader',
@@ -76,18 +65,8 @@ const originalConfig=module.exports = {
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
       }
-    ],
-    resolve: {
-      alias: {
-        'muse-components': 'muse-ui/src'
-      }
-    }
+    ]
   }
 }
 
-const webpackConfig = originalConfig;
-
-module.exports = vuxLoader.merge(webpackConfig, {
-  plugins: ['vux-ui']
-});
 
