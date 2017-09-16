934d17dfe795ed1d50185f184cc21410732289f0
lihongmi <741205360@qq.com>
2017-09-16 14:19:43 +0800

    wanmei

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,19 +1,18 @@
 <template>
-<div>
- 
-  <router-view></router-view>
+  <div id="app">
+    <router-view></router-view>
   
 
-  <mu-paper id="footer">
-  <mu-bottom-nav :value="bottomNav" @change="handleChange">
-    <mu-bottom-nav-item value="/home" title="首页" icon="home"/>
-    <mu-bottom-nav-item value="/recent" title="附近" icon="pin_drop"/>
-    <mu-bottom-nav-item value="/guang" title="逛一逛" icon="language"/>
-    <mu-bottom-nav-item value="/order" title="订单" icon="assignment"/>
-     <mu-bottom-nav-item value="/me" title="我的" icon="account_circle"/>
-  </mu-bottom-nav>
-</mu-paper>
-</div>
+      <mu-paper id="footer">
+      <mu-bottom-nav :value="bottomNav" @change="handleChange">
+        <mu-bottom-nav-item value="/home" title="首页" icon="home"/>
+        <mu-bottom-nav-item value="/recent" title="附近" icon="pin_drop"/>
+        <mu-bottom-nav-item value="/guang" title="逛一逛" icon="language"/>
+        <mu-bottom-nav-item value="/order" title="订单" icon="assignment"/>
+        <mu-bottom-nav-item value="/me" title="我的" icon="account_circle"/>
+      </mu-bottom-nav>
+    </mu-paper>
+  </div>
 </template>
 
 <script>
@@ -26,7 +25,6 @@ import BottomNavItem from 'muse-components/bottomNav/BottomNavItem'
 Vue.component(Paper.name, Paper)
 Vue.component(BottomNav.name, BottomNav)
 Vue.component(BottomNavItem.name, BottomNavItem)
-
 export default {
   data () {
     return {
@@ -42,18 +40,14 @@ export default {
     }
     
   }
-
-
 }
 </script>
 
-
-<style scope>
-    #footer{
-      position:fixed;
-      left:0;
-      bottom:0;
-      right:0;
-      z-index:1;
-    }
+<style>
+#footer {
+  position:absolute;
+  left:0;
+  right:0;
+  bottom:0;
+}
 </style>
