1bfa136990843a14cd983859f60a391bebacf919
qc777777 <895359039@qq.com>
2017-09-18 00:15:45 +0800

    aaaaa

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,48 +1,4 @@
 <template>
-<<<<<<< HEAD
- <div>
-  
-  <mt-tabbar v-model="selected">  
-    <mt-tab-item id="/">
-      <img slot="icon" src="./assets/logo.png">
-      首页
-    </mt-tab-item>
-    <mt-tab-item id="/about">
-      <img slot="icon" src="./assets/logo.png">
-      附近
-    </mt-tab-item>
-    <mt-tab-item id="/stroll">
-      <img slot="icon" src="./assets/logo.png">
-      逛一逛
-    </mt-tab-item>
-    <mt-tab-item id="/order">
-      <img slot="icon" src="./assets/logo.png">
-      订单
-    </mt-tab-item>
-    <mt-tab-item id="/my">
-      <img slot="icon" src="./assets/logo.png">
-      我的
-    </mt-tab-item>
-  </mt-tabbar>
-
-  <router-view></router-view>
-</div>
-</template>
-
-<script>
-
-export default {     
-  data(){
-      return { selected: '/'}
-  },
-  //监控属性
-  watch: {
-    selected (val) {
-      this.$router.push(val);
-    }
-  }, 
- 
-=======
   <div >
     <mu-paper id = "footer">
       <mu-bottom-nav :value="bottomNav" @change="handleChange">
@@ -70,16 +26,10 @@ export default {
       this.$router.push(val);
     }
   }
->>>>>>> bf97d071776b89ddeaf5058ed8beb63ceb59561e
 }
 
 </script>
 
-<<<<<<< HEAD
-<style scope>
-
-</style>
-=======
 <style>
 #footer {
   position:fixed;
@@ -88,4 +38,3 @@ export default {
   right:0;
 }
 </style>
->>>>>>> bf97d071776b89ddeaf5058ed8beb63ceb59561e
