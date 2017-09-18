cacc6f870410cd6ad4b146b22523b396742f24cf
wujian1994 <wjaiy_vip@163.com>
2017-09-19 00:30:50 +0800

    提交附近一点

--- app/src/pages/Nearby/Nearby.vue
+++ app/src/pages/Nearby/Nearby.vue
@@ -1,77 +1,23 @@
 <template>
   <div>
-    <mu-appbar id="yanse">
-      <mu-flat-button icon="expand_more" label="保定" class="demo-flat-button" labelPosition="before" iconButton />
-      <mu-text-field icon="search" class="appbar-search-field"   hintText="自助" fullWidth/>
-    </mu-appbar>
-      1</br>
-      2</br>
-      3</br>
-      4</br>
-      5</br>
-      6</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
-      1</br>
+  <search></search>
+  <loading></loading>
+    
   </div>
 </template>
 
 <script>
-   export default {
-        name:'search'
-    }
+import Search from './Search'
+import Loading from './Loading'
+export default {
+  name: 'home',
+  components:{
+    Search,
+    Loading
+  }
+}
 </script>
 
 <style lang="less">
-.appbar-search-field{
-  color: #FFF;
-  margin-bottom: 0;
-  &.focus-state {
-    color: #8E8E8E;
-  }
-  .mu-text-field-hint {
-    color: fade(#8E8E8E, 54%);
-  }
-  .mu-text-field-input {
-    color: #FFF;
-  }
-  .mu-text-field-focus-line {
-    background-color: #FFF;
-  }
-}
-#yanse{
-  background:	#FCFCFC;
-  color:#8E8E8E;
-  position:fixed;
-  left:0;
-  top:0;
-  right:0;
-}
+
 </style>
---
+++ app/src/pages/Nearby/Search.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <mu-appbar id="yanse">
+      <mu-flat-button icon="expand_more" label="保定" class="demo-flat-button" labelPosition="before" iconButton />
+      <mu-text-field icon="search" class="appbar-search-field"   hintText="自助" fullWidth/>
+    </mu-appbar>
+      
+  </div>
+</template>
+
+<script>
+   export default {
+        name:'search'
+    }
+</script>
+
+<style lang="less">
+.appbar-search-field{
+  color: #FFF;
+  margin-bottom: 0;
+  &.focus-state {
+    color: #8E8E8E;
+  }
+  .mu-text-field-hint {
+    color: fade(#8E8E8E, 54%);
+  }
+  .mu-text-field-input {
+    color: #FFF;
+  }
+  .mu-text-field-focus-line {
+    background-color: #FFF;
+  }
+}
+#yanse{
+  background:	#FCFCFC;
+  color:#8E8E8E;
+  position:fixed;
+  left:0;
+  top:0;
+  right:0;
+}
+</style>
